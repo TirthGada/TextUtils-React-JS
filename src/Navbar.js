382c39b2d033e5a17a8a7640a2844500391ec17a
Tirth <tirth@Tirths-MacBook-Air.local>
2023-05-05 15:16:28 +0530

    Added button of copying textto clickboard

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,9 +11,8 @@ export default function Navbar(props){
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-link active" aria-current="page" href="#">{props.title}</a>
-      <a class="nav-link" href="#">Features</a>
-      <a class="nav-link" href="#">Pricing</a>
-      <a class="nav-link disabled">Disabled</a>
+      <a class="nav-link" href="#">Home</a>
+      <a class="nav-link" href="https://github.com/TirthGada">About Me</a>
     </div>
   </div>
 </div>
--- src/Textform.js
+++ src/Textform.js
@@ -11,6 +11,10 @@ export default function Textform() {
 
   }
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(Text); 
+    alert("Copied");
+}
   const handleLowercaseClick=()=>{
     console.log("Lowercase clicked");
     let Newtext1=Text.toLowerCase();
@@ -30,12 +34,22 @@ export default function Textform() {
   return (
     <>
 <div className="mb-3">
-  <textarea class="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="18"></textarea>
+  <textarea class="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
 <button className="btn btn-primary mx-3" onClick={handleUppercaseClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-3" onClick={handleLowercaseClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-3" onClick={handleCopy}>Copy To Clipboard</button>
 <button className="btn btn-danger mx-3" onClick={handleclear}>Clear Text</button>
+<div className='container my-3 px-3'>
+<h2>Your Text Summary</h2>
 <p>{Text.length} Characters</p>
+</div>
+<div className='container'>
+  <h3>Preview</h3>
+  <div className='container border '><p>{Text}</p></div>
+ 
+</div>
+
  </>
   )
 }
